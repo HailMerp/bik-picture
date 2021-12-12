19e570c39bec8bed2d39ba06e678c8ce751d306e
Merp <nadia.ruszkiewicz@gmail.com>
2021-12-12 09:20:22 +0100

    heroku related changes

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
 const express = require('express');
+const path = require('path');
 const app = express();
-
-app.use(requireHTTPS);
-app.use(express.static('./dist/bik-picture'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/bik-picture/'}),
-);
-
+app.use(express.static(__dirname + '/dist/bik-picture'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/bik-picture/index.html'));});
 app.listen(process.env.PORT || 8080);
